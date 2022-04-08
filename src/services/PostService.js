952f00bf9c8dbfcc9edd6a33b3438d2dabeebe0f
ETTARAK <ettarak.zouhair@gmail.com>
2022-04-08 13:59:29 +0000

    Add services class

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -1,3 +1,13 @@
 import axios from "axios"
 
-const apiClient = axios.create('http://localhost:5000');
+const apiClient = axios.create({
+    baseURL: 'http://localhost:5000',
+    withCredentials: false,
+    headers: {
+        'Accept': 'application/json'
+    }
+});
+
+const getPosts = apiClient.get('/posts');
+
+export default  getPosts
